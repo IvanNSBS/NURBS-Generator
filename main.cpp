38a381bd9859f6ad9cf5e9365cbad4fdeaad12e7
Ivan Neves <insbs@cin.ufpe.br>
2018-11-26 08:35:59 -0300

    Falta agora fazer o leitor de arquivo.

--- main.cpp
+++ main.cpp
@@ -1,156 +1,83 @@
 #include "camera.h"
-#include <fstream> 
+#include "nurbs.h"
+#include <fstream>
 
-#define M_PI 3.141592653589793
-
-int factorial(int n){
-  return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n;
-}
-
-void getPoint(int offset, int n, float &t, vec3 *cp, vec3 &p){
-    p = vec3(0.0, 0.0, 0.0);
-    vec3 u = vec3(0.0, 0.0, 0.0);
-    vec3 d = vec3(0.0, 0.0, 0.0);
-    for(int i = 0; i <= n; ++i){
-        p += ( factorial(n)/ (factorial(i)*factorial(n-i)) ) * std::pow(t,i)*std::pow((1-t),(n-i))*cp[i+offset];
-    }
-    return;
-}
-
-void render_control_points(int min, int max, vec3 *cp, vec2i *rcp, camera &cam, std::ofstream &ofs){
-    for(int i = min; i < max; ++i){
-        bool visible0 = cam.compute_pixel_coordinates(cp[i], rcp[i]);
-        bool visible1 = cam.compute_pixel_coordinates(cp[i+1], rcp[i+1]);
-        ofs << "    <line x1=\"" << rcp[i].x() << "\" y1=\"" << rcp[i].y() << "\" x2=\"" << rcp[i+1].x() << "\" y2=\"" << rcp[i+1].y() << "\" style=\"stroke:rgb(255,0,0);stroke-width:1.2\" />\n";
-    }
-    return;
-}
-
-void render_bezier_curve(int nSegments, int n, vec3 *cp, camera &cam, std::ofstream &ofs){
-    vec3 *rp = new vec3[nSegments];
-    vec2i *rpc = new vec2i[nSegments];
-    for (int i = 0; i < nSegments; ++i) { 
-        float t0 = i / (float)nSegments; 
-        float t1 = (i+1) / (float)nSegments; 
-        getPoint(0, n, t0, cp, rp[i]);
-        getPoint(0, n, t1, cp, rp[i+1]);
-        bool visible = cam.compute_pixel_coordinates(rp[i], rpc[i]);
-        bool visible1 = cam.compute_pixel_coordinates(rp[i+1], rpc[i+1]);
-        ofs << "    <line x1=\"" << rpc[i].x() << "\" y1=\"" << rpc[i].y() << "\" x2=\"" << rpc[i+1].x() << "\" y2=\"" << rpc[i+1].y() << "\" style=\"stroke:rgb(255,0,0);stroke-width:1.2\" />\n"; 
-    }
-    return;
-}
-
-void render_surface_cp(int &n, vec3 *cp, camera &cam, std::ofstream &ofs){
-    int size = (n*n) - 1;
-    vec2i *rcp = new vec2i[size + 1];
-
-    for(int i = 0; i < size; i+=n){
-        int max = i+3;
-        render_control_points(i, max, cp, rcp, cam, ofs);
-    }
-
-    int lig = (n*(n-1));
-    for(int i = 0; i < lig; i++)
-        ofs << "    <line x1=\"" << rcp[i].x() << "\" y1=\"" << rcp[i].y() << "\" x2=\"" << rcp[i+n].x() << "\" y2=\"" << rcp[i+n].y() << "\" style=\"stroke:rgb(255,0,0);stroke-width:1.2\" />\n";
-}
-
-void render_bezier_surface(int nSegments, int n, vec3 *cp, camera &cam, std::ofstream &ofs){
-    int size = (nSegments+1)*(n+1);
-
-    vec2i *rpc = new vec2i[size];
-
-    int aux = 0;
-    for(int j = 0; j <= n; j++)
-    {
-        vec3 *rp = new vec3[nSegments+1];
-        for (int i = 0; i < nSegments; ++i) { 
-            float t0 = i / (float)nSegments; 
-            float t1 = (i+1) / (float)nSegments; 
-            int index = i + (j*(nSegments+1));
-            getPoint((n+1)*j, n, t0, cp, rp[i]);
-            getPoint((n+1)*j, n, t1, cp, rp[i+1]);
-            bool visible = cam.compute_pixel_coordinates(rp[i], rpc[index]);
-            bool visible1 = cam.compute_pixel_coordinates(rp[i+1], rpc[index+1]);
-            aux++;
-            ofs << "    <line x1=\"" << rpc[index].x() << "\" y1=\"" << rpc[index].y() << "\" x2=\"" << rpc[index+1].x() << "\" y2=\"" << rpc[index+1].y() << "\" style=\"stroke:rgb(0,0,0);stroke-width:1.2\" />\n"; 
-        }
-    }
-
-    int o = nSegments+1;
-    for(int i = 0; i < (nSegments+1)*n; i++)
-    {
-        ofs << "    <line x1=\"" << rpc[i].x() << "\" y1=\"" << rpc[i].y() << "\" x2=\"" << rpc[i+o].x() << "\" y2=\"" << rpc[i+o].y() << "\" style=\"stroke:rgb(0,0,0);stroke-width:1.2\" />\n";
-        ofs << "    <circle cx=\"" << rpc[i].x() << "\" cy=\"" << rpc[i].y() << "\" r=\"2.5\" stroke=\"black\" stroke-width=\"1\" fill=\"blue\" />" << std::endl;
-        ofs << "    <circle cx=\"" << rpc[i+o].x() << "\" cy=\"" << rpc[i+o].y() << "\" r=\"2.5\" stroke=\"black\" stroke-width=\"1\" fill=\"blue\" />" << std::endl;
-    }
-}
-
-int main(){
-
-    vec3 from(35,45,20);
-    vec3 at(40,0,-1);
-    float FoV = 60;
-    float near = 1;
-    camera cam(from, at, vec3(0,1,0), FoV, near, 800, 400);
-
+int main()
+{
+    //vec3 from(10,45,20);
+    //vec3 at(15,0,-1);
+    vec3 from(-28, 40, 10);
+    vec3 at(-10, 27, -1);
+    float FoV = 30;
+    float near = 1.2;
+    camera *cam = new camera(from, at, vec3(0,1,0), FoV, near, 400, 300);
     std::ofstream ofs; 
-    ofs.open("./bezierSurface.svg"); 
-    ofs << "<svg version=\"1.1\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns=\"http://www.w3.org/2000/svg\" width=\"" << cam.imgWidth << "\" height=\"" << cam.imgHeight << "\">" << std::endl;
-    ofs << "    <rect width=\"" << cam.imgWidth << "\" height=\"" << cam.imgHeight << "\" stroke=\"black\" stroke-width=\"0\" fill=\"rgb(120,120,120)\"/>\n" ; 
+    ofs.open("./bspline2.svg"); 
+    ofs << "<svg version=\"1.1\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns=\"http://www.w3.org/2000/svg\" width=\"" << cam->imgWidth << "\" height=\"" << cam->imgHeight << "\">" << std::endl;
+    ofs << "    <rect width=\"" << cam->imgWidth << "\" height=\"" << cam->imgHeight << "\" stroke=\"black\" stroke-width=\"0\" fill=\"rgb(150,150,150)\"/>\n" ; 
+       
+    vec3 *cpts1 = new vec3[16];
+    float kt[28] = {2, 2.1, 2.3, 2.8, 3.4, 3.5, 3.8,
+                    2, 2.1, 2.3, 2.8, 3.4, 3.5, 3.8,
+                    2, 2.1, 2.3, 2.8, 3.4, 3.5, 3.8,
+                    2, 2.1, 2.3, 2.8, 3.4, 3.5, 3.8};
+                    
+    float weights[16] = {0.2, 0.2, 0.2, 0.2,
+                         2, 0.55, 0.55, 2,
+                         2, 0.55, 0.55, 2,
+                         0.2, 0.2, 0.2, 0.2};
 
-    int pnum = 4;
-    int pnum2 = pnum*pnum;
-    int n = pnum-1;
-    int numSegments = 30;
-    vec3 *cp = new vec3[pnum];
-    vec3 *cp2 = new vec3[pnum];
-    vec3 *cp3 = new vec3[pnum];
-    vec3 *cp4 = new vec3[pnum];
-    vec3 *scp2 = new vec3[pnum2];
+    cpts1[0] = vec3(2, 11, -7);
+    cpts1[1] = vec3(5, 11, -7);
+    cpts1[2] = vec3(8, 11, -7);
+    cpts1[3] = vec3(11, 11, -7);
 
-    cp[0] = scp2[0] = vec3(20.0, 0, -10);
-    cp[1] = scp2[1] = vec3(40.0, 13, -15);
-    cp[2] = scp2[2] = vec3(60.0, 12, -13);
-    cp[3] = scp2[3] = vec3(80.0, 0, -10);
+    cpts1[4] = vec3(2, 11, -10);
+    cpts1[5] = vec3(5, 15, -10);
+    cpts1[6] = vec3(8, 15, -10);
+    cpts1[7] = vec3(11, 11, -10);
 
-    cp2[0] = scp2[4] = vec3(20.0, 0, 0);
-    cp2[1] = scp2[5] = vec3(40.0, 17, -5);
-    cp2[2] = scp2[6] = vec3(58.0, 15, -3);
-    cp2[3] = scp2[7] = vec3(75.0, 0, 2);
+    cpts1[8] = vec3(2, 11, -13);
+    cpts1[9] = vec3(5, 15, -13);
+    cpts1[10] = vec3(8, 15, -13);
+    cpts1[11] = vec3(11, 11, -13);
 
-    cp3[0] = scp2[8] = vec3(20.0, 0, 10);
-    cp3[1] = scp2[9] = vec3(40.0, 16, 5);
-    cp3[2] = scp2[10] = vec3(57.0, 14.5, 7);
-    cp3[3] = scp2[11] = vec3(73.0, 0, 13);
+    cpts1[12] = vec3(2, 11, -16);
+    cpts1[13] = vec3(5, 11, -16);
+    cpts1[14] = vec3(8, 11, -16);
+    cpts1[15] = vec3(11, 11, -16);
 
-    cp4[0] = scp2[12] = vec3(20.0, 0, 20);
-    cp4[1] = scp2[13] = vec3(40.0, 3, 15);
-    cp4[2] = scp2[14] = vec3(60.0, 1.5, 17);
-    cp4[3] = scp2[15] = vec3(75.0, 0, 23);
-
-    for(int i = 0; i < pnum2; ++i)
-    {
-        matrix44 tr(1,0,0,0,
-                    0,1,0,0,
-                    0,0,1,0,
-                    -20,0,10,1);
-        matrix44 itr = tr.inverse();
-        float sen30 = 0.5;
-        float cos30 = 0.86602540378;
-        matrix44 rot(cos30, 0, -sen30, 0, 
-                       0,   1,    0,   0,
-                     sen30, 0, cos30, 0, 
-                       0,   0,    0,   1);
-        matrix44 result = (tr*rot)*itr;
-        vec3 aux;
-        result.multVecMatrix(scp2[i], aux);
-        scp2[i] = aux;
-    }
-    
-    render_bezier_surface(numSegments, n, scp2, cam, ofs);
-    render_surface_cp(pnum, scp2, cam, ofs);
-
+    NURBS n(3, 3, cpts1, weights, kt);
+    n.cam = cam;
+    //n.rot_x(50);
+    //n.rot_z(30);
+    //n.rot_y(-30);
+    n.render_curve(ofs, 20);
+    n.render_control_points(ofs);
+    n.render_bounding_box(ofs);
     ofs << "</svg>";
+
     return 0;
 }
+
+    /*cpts[0] = vec3(2, 13, -10);
+    cpts[1] = vec3(2, 16, -10);
+    cpts[2] = vec3(8, 16, -10);
+    cpts[3] = vec3(8, 12, -10);
+    cpts[4] = vec3(12, 15, -10);
+    cpts[5] = vec3(14, 12, -10);
+
+    cpts[0] = vec3(2, 13, -10);
+    cpts[1] = vec3(2, 20, -10);
+    cpts[2] = vec3(8, 20, -10);
+    cpts[3] = vec3(8, 11, -10);
+    cpts[4] = vec3(4, 11, -10);
+    cpts[5] = vec3(4, 15, -10);*/
+
+    /*cpts[0] = vec3(2, 27, -10);
+    cpts[1] = vec3(6, 4, -10);
+    cpts[2] = vec3(12, 13, -10);
+    cpts[3] = vec3(18.5, 18, -10);
+    cpts[4] = vec3(26, 9, -10);
+    cpts[5] = vec3(34.5, 14, -10);
+    cpts[6] = vec3(34, 21, -10);*/
--- nurbs.h
+++ nurbs.h
@@ -6,9 +6,18 @@
 #include <fstream>
 #include <vector>
 #include <limits>
+#include <math.h>
 
 #define MIN 0
 #define MAX 1
+#define PI 3.14159265
+
+static const float FMAX = std::numeric_limits<float>::max();
+
+struct surface{
+    vec3 point;
+    vec3 normal;
+};
 
 class NURBS
 {
@@ -25,7 +34,6 @@ public:
           vec3 *cps = 0, float *w = 0, float *kts = 0):
           n(nn), order(ord), m(nn + ord + 1), controlPoints(cps), weights(w), knots(kts)
     {
-        float FMAX = std::numeric_limits<float>::max();
         min_max[MIN] = vec3(FMAX, FMAX, FMAX);
         min_max[MAX] = vec3(-9999999, -9999999, -9999999);
 
@@ -43,6 +51,7 @@ public:
             for(int i = 0; i < n+1; i++)
                 weights[i] = 1;
         }
+        
         for(int i = 0; i <= n; i++)
             for(int j = 0; j <= n; j++)
                 eval_min_max(controlPoints[(i*(n+1))+j]);
@@ -68,7 +77,7 @@ public:
         if(p.z() > min_max[MAX].z())
             min_max[MAX][2] = p.z();
 
-        //std::cout << "min-> " << min_max[min] << " :::  max->" << min_max[max] << std::endl;
+        //std::cout << "min-> " << min_max[MIN] << " :::  max->" << min_max[MAX] << std::endl;
     }
 
     vec3 get_box_center(){
@@ -113,7 +122,66 @@ public:
         }
     }
 
-    vec3 deBoor(int n, int k, float s, float t){
+    float d_S(int i, int k, float t){
+        int index = i-1;
+        if(k == 1){
+            if( t >= knots[index] && t < knots[index + 1])
+                return 1.0;
+            else
+                return 0.0;
+        }
+        else{
+            float numeratorA = (k);
+            float denominatorA = (knots[index+k-1]-knots[index]);
+            float numeratorB = (k);
+            float denominatorB = (knots[index+k]-knots[index+1]);
+
+            float rA = 0;
+            float rB = 0;
+
+            if( denominatorA != 0 ){
+                rA = (numeratorA/denominatorA) * d_S(i, k-1, t) ;
+            }
+            
+            if( denominatorB != 0){
+                rB = (numeratorB/denominatorB) * d_S(i+1, k-1, t);
+            }
+
+            return rA - rB;
+        }
+    }
+
+    vec3 get_tg_s(int k, float s, float t){
+        vec3 result(0.0, 0.0, 0.0);
+        for(int i = 1; i <= n+1; i++)
+        {
+            for(int j = 1; j <= n+1; j++)
+            {
+                float w1 = S(j, k, t);
+                float w2 = d_S(i, k, s);
+                vec3 numerator = w2 * w1 * controlPoints[j-1 + ((n+1)*(i-1)) ];
+                result += numerator;
+            }
+        }
+        return result;
+    }
+
+    vec3 get_tg_t(int k, float s, float t){
+        vec3 result(0.0, 0.0, 0.0);
+        for(int i = 1; i <= n+1; i++)
+        {
+            for(int j = 1; j <= n+1; j++)
+            {
+                float w1 = d_S(j, k, t);
+                float w2 = S(i, k, s);
+                vec3 numerator = w2 * w1 * controlPoints[j-1 + ((n+1)*(i-1)) ];
+                result += numerator;
+            }
+        }
+        return result;
+    }
+
+    vec3 eval_surface(int k, float s, float t){
         vec3 result(0.0, 0.0, 0.0);
         float denom = 0;
 
@@ -162,7 +230,6 @@ public:
     }
 
     void render_curve(std::ofstream &ofs, int numSegments){
-        std::vector<vec2f> samples;
         for(int i = 0; i < numSegments - 1; i++)
         {
             float t = (i/(float)numSegments)*((n+1) - (order-1)) + (order-1) + 0.03;
@@ -173,10 +240,10 @@ public:
                 float s1 = (j/(float)numSegments)*((n+1) - (order-1)) + (order-1) + 0.03;
                 float s2 = ((j+1)/(float)numSegments)*((n+1) - (order-1)) + (order-1) + 0.03;
 
-                if( (s1 < knots[m-1] && s2 < knots[m-1] && t < knots[m-1]) )
+                if( (s1 <= knots[m-1] && s2 <= knots[m-1] && t <= knots[m-1]) )
                 {
-                    vec3 pt1 = deBoor(n, order, t, s1);
-                    vec3 pt2 = deBoor(n, order, t, s2);
+                    vec3 pt1 = eval_surface(order, t, s1);
+                    vec3 pt2 = eval_surface(order, t, s2);
                     vec2f pr1;
                     vec2f pr2;
                     cam->compute_pixel_coordinates(pt1, pr1);
@@ -184,10 +251,10 @@ public:
                     //ofs << "    <circle cx=\"" << pr1.x() << "\" cy=\"" << pr1.y() << "\" r=\"0.8\" stroke=\"black\" stroke-width=\"0.3\" fill=\"red\" />" << std::endl;
                     ofs << "    <line x1=\"" << pr1.x() << "\" y1=\"" << pr1.y() << "\" x2=\"" << pr2.x() << "\" y2=\"" << pr2.y() << "\" style=\"stroke:rgb(0,25,115);stroke-width:0.8\" />\n"; 
 
-                    if(t1 < knots[m-1])
+                    if(t1 <= knots[m-1])
                     {
-                        vec3 pn1 = deBoor(n, order, t1, s1);
-                        vec3 pn2 = deBoor(n, order, t1, s2);
+                        vec3 pn1 = eval_surface(order, t1, s1);
+                        vec3 pn2 = eval_surface(order, t1, s2);
                         vec2f prn1;
                         vec2f prn2;
                         cam->compute_pixel_coordinates(pn1, prn1);
@@ -202,6 +269,12 @@ public:
 
     void render_bounding_box(std::ofstream &ofs){
         //compute x
+        /*min_max[MIN] = vec3(FMAX, FMAX, FMAX);
+        min_max[MAX] = vec3(-9999999, -9999999, -9999999);
+        for(int i = 0; i < (n+1)*(n+1); i++){
+            eval_min_max(controlPoints[i]);
+        }*/
+
         vec2f pZmin1;
         vec2f pZmin2;
         cam->compute_pixel_coordinates(vec3(min_max[MIN].x(), min_max[MIN].y(), min_max[MIN].z()), pZmin1);
@@ -247,6 +320,75 @@ public:
         ofs << "    <line x1=\"" << pXZmax1.x() << "\" y1=\"" << pXZmax1.y() << "\" x2=\"" << pYmax1.x() << "\" y2=\"" << pYmax1.y() << "\" style=\"stroke:rgb(0,255,0);stroke-width:1.2\" />\n"; 
         ofs << "    <line x1=\"" << pXZmax2.x() << "\" y1=\"" << pXZmax2.y() << "\" x2=\"" << pYmax2.x() << "\" y2=\"" << pYmax2.y() << "\" style=\"stroke:rgb(0,255,0);stroke-width:1.2\" />\n"; 
         
+    }
+
+    void rot_x(float deg){
+
+        vec3 point = get_box_center();
+        matrix44 tr(1,0,0,0,
+                    0,1,0,0,
+                    0,0,1,0,
+                    -point.x(),-point.y(),-point.z(),1);
+        matrix44 itr = tr.inverse();
+        float sen = sin(deg*PI/180.0);
+        float co = cos(deg*PI/180);
+        matrix44 rot( 1,   0,   0,   0, 
+                      0,  co,-sen,   0,
+                      0, sen,  co,   0, 
+                      0,   0,   0,   1);
+        matrix44 result = (tr*rot)*itr;
+
+        for(int i = 0; i < ((n+1)*(n+1)); ++i)
+            result.multVecMatrix(controlPoints[i], controlPoints[i]);
+
+        result.multVecMatrix(min_max[MIN], min_max[MIN]);
+        result.multVecMatrix(min_max[MAX], min_max[MAX]);
+
+    }
+
+    void rot_y(float deg){
+
+        vec3 point = get_box_center();
+        matrix44 tr(1,0,0,0,
+                    0,1,0,0,
+                    0,0,1,0,
+                    -point.x(),-point.y(),-point.z(),1);
+        matrix44 itr = tr.inverse();
+        float sen = sin(deg*PI/180.0);
+        float co = cos(deg*PI/180);
+        matrix44 rot( co,   0, sen, 0, 
+                        0,   1,   0, 0,
+                    -sen,   0,  co, 0, 
+                        0,   0,   0, 1);
+        matrix44 result = (tr*rot)*itr;
+
+        for(int i = 0; i < ((n+1)*(n+1)); ++i){
+            result.multVecMatrix(controlPoints[i], controlPoints[i]);
+        }
+
+        result.multVecMatrix(min_max[MIN], min_max[MIN]);
+        result.multVecMatrix(min_max[MAX], min_max[MAX]);
+    }
+
+    void rot_z(float deg){
+
+        vec3 point = get_box_center();
+        matrix44 tr(1,0,0,0,
+                    0,1,0,0,
+                    0,0,1,0,
+                    -point.x(),-point.y(),-point.z(),1);
+        matrix44 itr = tr.inverse();
+        float sen = sin(deg*PI/180.0);
+        float co = cos(deg*PI/180);
+        matrix44 rot( co, -sen,   0,   0, 
+                         sen,   co,   0,   0,
+                           0,    0,   1,   0, 
+                           0,    0,   0,   1);
+        matrix44 result = (tr*rot)*itr;
+
+        for(int i = 0; i < ((n+1)*(n+1)); ++i){
+            result.multVecMatrix(controlPoints[i], controlPoints[i]);
+        }
     }
 };
 
